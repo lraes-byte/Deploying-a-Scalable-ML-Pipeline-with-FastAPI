33be8dea63aa31f4759ebc3e2c481aa2af773e3e
Lauren R <lraes@wgu.edu>
2025-07-21 15:24:11 -0700

    Working on Step 4

--- train_model.py
+++ train_model.py
@@ -13,14 +13,14 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "Your path here"
+project_path = os.getcwd()
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = None # your code here
+data = pd.read_csv(data_path)
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = None, None# Your code here
+train, test = train_test_split(data, test_size=0.20, random_state=42)
 
 # DO NOT MODIFY
 cat_features = [
@@ -40,6 +40,9 @@ X_train, y_train, encoder, lb = process_data(
     # use the train dataset 
     # use training=True
     # do not need to pass encoder and lb as input
+    train,
+    categorical_features=cat_features,
+    label='salary'
     )
 
 X_test, y_test, _, _ = process_data(
